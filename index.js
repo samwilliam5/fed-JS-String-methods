c756bdf02207d45c8d90a9336b338fed5f43bac1
william5 <samwilliamj333@gmail.com>
2022-09-26 10:58:11 +0530

    add

---
+++ index.js
@@ -0,0 +1,62 @@
+//to lowercase()
+let lowercase='LOWERCASE'
+console.log(`lowercase:`,lowercase.toLowerCase());
+
+
+//to uppercase()
+let uppercase='uppercase'
+console.log(`uppercase:`,uppercase.toUpperCase());
+
+//repeat
+let repeat='hello '
+console.log(`Repeat:`,repeat.repeat(5));
+
+//replace
+let replace='hi'
+console.log(`Replace:`,replace.replace('hi','welcome'));
+
+
+//check string ends with
+let endswith='sdsdsdsbbshdbsbD'
+console.log(`EndsWith:`,endswith.endsWith('d'));
+
+//includes
+let includes='include'
+console.log(`Includes:`,includes.includes('z'));
+
+//concat
+let user='welcome';
+let user1=' ALL';
+console.log(`Concat:`,user.concat(user1));
+
+//indexOf
+let users='Welcome'
+console.log(`Index Of m:`,users.indexOf('m'));
+
+//split
+let fruits3='apple'
+let fruits4=fruits3.split('')
+console.log(`Split:`,...fruits4);
+
+//slice startindex endindex
+let who='worldHealthOranization '
+console.log(`Slice:`,who.slice(2,8));
+
+
+//serach
+let search='hello all'
+console.log(`Search:`,search.search('welcome'));
+
+// indexOfCharacter
+let fruits='apple'
+console.log('Index of character:',fruits.charAt(0));
+
+
+
+
+
+
+
+
+
+
